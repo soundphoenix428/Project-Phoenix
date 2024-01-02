526c5bbdfc5b9282bd4de8f7f064a24db2fe235f
Tanner S <135170881+soundphoenix428@users.noreply.github.com>
2024-01-02 09:54:57 -0500

    Add warn command

--- src/commands/chat/Utilities /Moderation/warn-command.ts
+++ src/commands/chat/Utilities /Moderation/warn-command.ts
@@ -1,45 +1,60 @@
-import { ChatInputCommandInteraction, EmbedBuilder, PermissionsString } from 'discord.js';
-
-import { HelpOption } from '../../enums/index.js';
+import { InfoOption } from '../../enums/index.js';
 import { Language } from '../../models/enum-helpers/index.js';
 import { EventData } from '../../models/internal-models.js';
 import { Lang } from '../../services/index.js';
-import { ClientUtils, FormatUtils, InteractionUtils } from '../../utils/index.js';
+import { InteractionUtils } from '../../utils/index.js';
 import { Command, CommandDeferType } from '../index.js';
+import { PingCommand } from './PingCommand';
+import { BanCommand } from './BanCommand';
+import { WarnCommand } from './WarnCommand'; // Import the WarnCommand
 
-export class WarnCommand implements Command {
-    public names = [Lang.getRef('chatCommands.help', Language.Default)];
+export class InfoCommand implements Command {
+    public names = [Lang.getRef('chatCommands.info', Language.Default)];
     public deferType = CommandDeferType.PUBLIC;
     public requireClientPerms: PermissionsString[] = [];
+
     public async execute(intr: ChatInputCommandInteraction, data: EventData): Promise<void> {
         let args = {
             option: intr.options.getString(
                 Lang.getRef('arguments.option', Language.Default)
-            ) as HelpOption,
+            ) as InfoOption,
         };
 
         let embed: EmbedBuilder;
         switch (args.option) {
-            case HelpOption.CONTACT_SUPPORT: {
-                embed = Lang.getEmbed('displayEmbeds.helpContactSupport', data.lang);
+            case InfoOption.ABOUT: {
+                embed = Lang.getEmbed('displayEmbeds.about', data.lang);
                 break;
             }
-            case HelpOption.COMMANDS: {
-                embed = Lang.getEmbed('displayEmbeds.helpCommands', data.lang, {
-                    CMD_LINK_TEST: FormatUtils.commandMention(
-                        await ClientUtils.findAppCommand(
-                            intr.client,
-                            Lang.getRef('chatCommands.test', Language.Default)
-                        )
-                    ),
-                    CMD_LINK_INFO: FormatUtils.commandMention(
-                        await ClientUtils.findAppCommand(
-                            intr.client,
-                            Lang.getRef('chatCommands.info', Language.Default)
-                        )
-                    ),
-                });
+            case InfoOption.TRANSLATE: {
+                embed = Lang.getEmbed('displayEmbeds.translate', data.lang);
+                for (let langCode of Language.Enabled) {
+                    embed.addFields([
+                        {
+                            name: Language.Data[langCode].nativeName,
+                            value: Lang.getRef('meta.translators', langCode),
+                        },
+                    ]);
+                }
                 break;
+            }
+            case InfoOption.PING: {
+                // Use the PingCommand for the ping functionality
+                const pingCommand = new PingCommand();
+                await pingCommand.execute(intr, data);
+                return;
+            }
+            case InfoOption.BAN: {
+                // Use the BanCommand for the ban functionality
+                const banCommand = new BanCommand();
+                await banCommand.execute(intr, data);
+                return;
+            }
+            case InfoOption.WARN: {
+                // Use the WarnCommand for the warn functionality
+                const warnCommand = new WarnCommand();
+                await warnCommand.execute(intr, data);
+                return;
             }
             default: {
                 return;
